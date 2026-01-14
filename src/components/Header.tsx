bf47bbce116f71c0658526e74a3673e6d53a1f42
Suppingly <kul1kov.alexandr@mail.ru>
2026-01-14 19:39:00 +0300

    Add files via upload

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+function Header() {
+    return (<>
+        <div className="header_container">
+            <div className="header_logo">Лого</div>
+            <div className="header_options">
+                <a href="#" className="header_option">Главная</a>
+                <a href="#" className="header_option">Функции</a>
+                <a href="#" className="header_option">Цены</a>
+                <a href="#" className="header_option">Блог</a>
+            </div>
+            <a href="#" className="header_link">Начать</a>
+        </div>
+    </>)
+}
+export default Header
